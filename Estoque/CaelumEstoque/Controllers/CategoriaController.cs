2ee5d4d3d315a3483ab2260417d67e6aa85c5e5e
Rafael Viegas <rafael.viegas@arpagus.com.br>
2016-12-29 16:56:08 -0200

    02. O padrão MVC 03.Controlando Requisições e a Listagem de Produtos

---
+++ Estoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using CaelumEstoque.DAO;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            var dao = new CategoriasDAO();
+            var categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+    }
+}
---
+++ Estoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            var dao = new ProdutosDAO();
+            var produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+            return View();
+        }
+    }
+}
