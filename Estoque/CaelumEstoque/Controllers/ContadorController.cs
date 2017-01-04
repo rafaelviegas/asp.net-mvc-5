f658c91ac442cf59342a909807680a772e6225d8
Rafael Viegas <rafael.viegas@arpagus.com.br>
2017-01-04 17:33:13 -0200

    10.Sessões para autenticar usuários

---
+++ Estoque/CaelumEstoque/Controllers/ContadorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ContadorController : Controller
+    {
+        // GET: Contador
+        public ActionResult Index()
+        {
+            var valorNaSession = Session["contador"];
+            var contador = Convert.ToInt32(valorNaSession);
+                contador++;
+
+            Session["contador"] = contador;
+
+            return View(contador);
+        }
+    }
+}
---
+++ Estoque/CaelumEstoque/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using CaelumEstoque.DAO;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(string login, string senha)
+        {
+            var dao = new UsuariosDAO();
+            var usuario = dao.Busca(login, senha);
+            if (usuario == null) return RedirectToAction("Index");
+
+            Session["UsuarioLogado"] = usuario;
+            return RedirectToAction("Index", "Produto");
+        }
+    }
+}
